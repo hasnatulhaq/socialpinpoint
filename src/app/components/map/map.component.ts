f7ff655dc5837cfd13add71b2006f6fde748a235
Hasnat ul haq <hasnatulhaq@outlook.com>
2024-03-04 22:29:08 +0500

    leaflet map added

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
+
 
 @Component({
   selector: 'app-map',
@@ -7,6 +9,23 @@ import { Component } from '@angular/core';
   templateUrl: './map.component.html',
   styleUrl: './map.component.scss'
 })
-export class MapComponent {
-      
+export class MapComponent implements OnInit, AfterViewInit {
+
+  private map: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    this.map = L.map('map').setView([51.505, -0.09], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(this.map);
+  }
 }
