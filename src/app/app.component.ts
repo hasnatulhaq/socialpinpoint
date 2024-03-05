b9add6825ae167fdd53ad8cf1a9a595efa03d1ca
Hasnat ul haq <hasnatulhaq@outlook.com>
2024-03-05 18:48:33 +0500

    changed in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,19 @@ import { RouterOutlet } from '@angular/router';
 import { MapComponent } from './components/map/map.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LeftSideNavComponent } from './components/left-side-nav/left-side-nav.component';
+import { CommentSearchComponent } from './components/comment-search/comment-search.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , MapComponent , HeaderComponent , LeftSideNavComponent ],
+  imports: [
+    RouterOutlet, 
+    MapComponent,
+    HeaderComponent, 
+    LeftSideNavComponent,
+    CommentSearchComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
