a6960399424ffa40bd9f646675285b4861d39b1c
Hasnat ul haq <hasnatulhaq@outlook.com>
2024-04-06 14:07:25 +0500

    Desined admins , plans , choose plan page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AdminComponent } from './pages/admin/admin.component';
+import { PlansComponent } from './pages/plans/plans.component';
+import { ChoosePlanComponent } from './pages/choose-plan/choose-plan.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {'path': 'admin' , component: AdminComponent},
+    {'path': 'plans' , component: PlansComponent},
+    {'path': 'choose-plan' , component: ChoosePlanComponent},
+];
